2129a332328c2475ac846277e74a59781a6208be
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2020-11-22 15:54:21 +0100

    setup base project

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World, I'm working with NEXT JS framework</h1>
+      <div className="container">
+        <h2>Subtitle</h2>
+      </div>
+    </div>
+  );
+}
