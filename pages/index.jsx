9214005c0902c645ffb1a72711a9fc0d25158898
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2021-01-25 18:31:56 +0100

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,82 +1,98 @@
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper";
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation } from 'swiper'
 
-SwiperCore.use(Navigation);
+SwiperCore.use(Navigation)
 
-export default function Home() {
+export default function Home () {
   return (
-    <div className="container">
-      <header className="header">
-        <div className="header__slider-container">
+    <div className='container'>
+      <header className='header'>
+        <div className='header__slider-container'>
           <Swiper
             navigation={{
-              nextEl: ".btn-slider--next",
-              prevEl: ".btn-slider--previous",
+              nextEl: '.btn-slider--next',
+              prevEl: '.btn-slider--previous'
             }}
             onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log("slide change")}
-            className="header__slider"
+            onSlideChange={() => console.log('slide change')}
+            className='header__slider'
           >
-            <SwiperSlide className="header__image-container">
+            <SwiperSlide className='header__image-container'>
               <Image
-                className="header__image"
-                src="/assets/desktop-image-hero-2.jpg"
-                alt="logo"
-                layout="fill"
-                objectFit="contain"
+                className='header__image'
+                src='/assets/image-hero-1.webp'
+                alt='chair wood'
+                layout='fill'
+                objectFit='cover'
+                objectPosition='center'
               />
             </SwiperSlide>
-            <SwiperSlide className="header__image-container">
+
+            <SwiperSlide className='header__image-container'>
               <Image
-                className="header__image"
-                src="/assets/desktop-image-hero-2.jpg"
-                alt="logo"
-                width={1260}
-                height={1010}
-                layout="responsive"
+                className='header__image'
+                src='/assets/image-hero-2.webp'
+                alt='chair black'
+                layout='fill'
+                objectFit='cover'
+                objectPosition='center'
+              />
+            </SwiperSlide>
+
+            <SwiperSlide className='header__image-container'>
+              <Image
+                className='header__image'
+                src='/assets/image-hero-3.webp'
+                alt='chair black'
+                layout='fill'
+                objectFit='cover'
+                objectPosition='center'
               />
             </SwiperSlide>
           </Swiper>
-          <div className="header__btn-container">
-            <button className="btn-slider btn-slider--previous">
+          <div className='header__btn-container'>
+            <button className='btn-slider btn-slider--previous'>
               previous
             </button>
-            <button className="btn-slider btn-slider--next">next</button>
+            <button className='btn-slider btn-slider--next'>next</button>
           </div>
         </div>
 
-        <div className="header__content">
-          <h1 className="header__title">
+        <div className='header__content'>
+          <h1 className='header__title'>
             Discover innovative ways to decorate
           </h1>
 
-          <p className="header__text">
+          <p className='header__text'>
             We provide unmatched quality, comfort, and style for property owners
             across the country. Our experts combine form and function in
             bringing your vision to life. Create a room in your own style with
             our collection and make your property a reflection of you and what
             you love.
           </p>
-          <a className="header__link" href="/">
+          <a className='header__link' href='/'>
             SHOP NOW
           </a>
         </div>
       </header>
 
-      <section className="about">
-        <div className="about__image-container about__image-container--left">
-          <img
-            className="about__image"
-            src="/assets/image-about-dark.jpg"
-            alt=""
+      <section className='about'>
+        <div className='about__image-container about__image-container--left'>
+          <Image
+            className='about__image'
+            src='/assets/image-about-dark.webp'
+            alt=''
+            layout='fill'
+            objectFit='cover'
+            objectPosition='center'
           />
         </div>
 
-        <div className="about__content">
-          <h2 className="about__title">About our furniture</h2>
+        <div className='about__content'>
+          <h2 className='about__title'>About our furniture</h2>
 
-          <p className="about__text">
+          <p className='about__text'>
             Our multifunctional collection blends design and function to suit
             your individual taste. Make each room unique, or pick a cohesive
             theme that best express your interests and what inspires you. Find
@@ -86,14 +102,17 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="about__image-container about__image-container--right">
-          <img
-            className="about__image"
-            src="/assets/image-about-light.jpg"
-            alt=""
+        <div className='about__image-container about__image-container--right'>
+          <Image
+            className='about__image'
+            src='/assets/image-about-light.webp'
+            alt=''
+            layout='fill'
+            objectFit='cover'
+            objectPosition='center'
           />
         </div>
       </section>
     </div>
-  );
+  )
 }
